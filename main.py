9fb4a1b881d05850796227fe06a3f03636699e4a
Jaewon Youn <78005200+larry1121@users.noreply.github.com>
2024-01-10 11:57:21 +0000

    keyword

--- main.py
+++ main.py
@@ -35,50 +35,22 @@ def get_category_id(label):
         if i['label'] == label:
             return i['id']
 
-def write_content(label):
-    if label == "MBTI":
-        response = ai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": SYSTEM},
-                    {"role": "user", "content": USER}
-                ],
-                temperature=1,
-                stream=True
-            )
-
-    writing = ""
-    for chunk in response:
-        if 'content' in chunk['choices'][0]['delta'].keys():
-            writing += chunk['choices'][0]['delta']['content']
-            print(chunk['choices'][0]['delta']['content'], end="")
-    print("")
-    return writing
-
-def get_hashtag(writing):
-    response = ai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": writing},
-                {"role": "user", "content": """위 블로그 글과 맞는 해시 태그 만들어줘. 쉼표로 구분하고 #제거
-해쉬 태그:"""}
-                ],
-            temperature=0,
-        )
-    return response['choices'][0]['message']['content']
-
-def sep_writing(writing):
-    title = writing[4:writing.find('\n')]
-    content = writing[writing.find('<'):]
-    return title, content
-if __name__ == "__main__":
-    label = "MBTI"
+def process_and_write_content(label, specific_MBTI):
     category = get_category_id(label)
-    writing = write_content(label)
+    writing = write_content(label, specific_MBTI)
     title, content = sep_writing(writing)
     print("title: " + title)
     print(content)
     tag = get_hashtag(writing)
     print(tag)
     write_blog(title, content, category, tag)
-    
+
+if __name__ == "__main__":
+    mbti_types = ["ENFJ", "ENFP", "ENTJ", "ENTP", "ESFJ", "ESFP", "ESTJ", "ESTP",
+                  "INFJ", "INFP", "INTJ", "INTP", "ISFJ", "ISFP", "ISTJ", "ISTP"]
+
+    for mbti_type in mbti_types:
+        label = "MBTI"
+        specific_MBTI = mbti_type
+        process_and_write_content(label, specific_MBTI)
+
